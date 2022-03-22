ddd847a431a5b1008ec5e4dc0bc38effd70bedb8
gurubamal <99092495+gurubamal@users.noreply.github.com>
2022-03-22 17:12:26 +0530

    Add files via upload

--- compute_add.sh
+++ compute_add.sh
@@ -1,6 +1,6 @@
 if ! [ $HOSTNAME = node6 ]; then
 	if ! [ -e /etc/kubernetes/kubelet.conf ] ; then
-	sudo kubeadm join 192.168.58.6:6443 --token p4bd45.a1dizj25wiidz3hs --discovery-token-ca-cert-hash sha256:c4a9ffa476ac106925fa1e5191767800eabdfda91a2ad3f91e80fb9741c4e980
+	sudo kubeadm join 192.168.58.6:6443 --token dt0n3u.u8cg014jh2hszfu2 --discovery-token-ca-cert-hash sha256:87ce34c89210c06fb53176cc1ba8caabc1e8e572768f1aa5c5420ea8f36de454
 	fi
 	else exit 0
-fi
+	fi
