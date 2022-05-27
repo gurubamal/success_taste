0ddff78ccce83321c77ce114106d102516f077a2
gurubamal <99092495+gurubamal@users.noreply.github.com>
2022-05-27 18:47:08 +0530

    Update final_touch.sh

--- final_touch.sh
+++ final_touch.sh
@@ -1,2 +1,2 @@
 vagrant ssh node6  -c '/vagrant/05_post_join_control.sh'
-vagrant ssh node6  -c 'kubectl apply -f https://projectcalico.docs.tigera.io/manifests/calico.yaml'
+#vagrant ssh node6  -c 'kubectl apply -f https://projectcalico.docs.tigera.io/manifests/calico.yaml'
