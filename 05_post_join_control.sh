d607074cc25e74de5e21ec83582d4c55d2afc07a
gurubamal <99092495+gurubamal@users.noreply.github.com>
2022-06-06 00:12:20 +0530

    Update 05_post_join_control.sh

--- 05_post_join_control.sh
+++ 05_post_join_control.sh
@@ -1,8 +1,8 @@
 #kubectl apply -f https://github.com/coreos/flannel/raw/master/Documentation/kube-flannel.yml
 #kubectl apply -f kubei.yaml
-if [ "$HOSTNAME" = node6 ]; then
-        if ! [ -e /home/vagrant/.kube/config ]
-        then
+#if [ "$HOSTNAME" = node6 ]; then
+#        if ! [ -e /home/vagrant/.kube/config ]
+#        then
         sudo mkdir -p /home/vagrant/.kube
 	sudo chown vagrant:vagrant /home/vagrant/.kube
         sudo cp -i /etc/kubernetes/admin.conf /home/vagrant/.kube/config
@@ -12,8 +12,8 @@ if [ "$HOSTNAME" = node6 ]; then
 	sudo snap install helm --classic
 #        kubectl apply -f https://projectcalico.docs.tigera.io/manifests/calico.yaml
 #sudo snap install helm --classic
-        else echo "Now Installing calico Netwrok Plugin......."
-        fi
-fi
+#       else echo "Now Installing calico Netwrok Plugin......."
+#        fi
+#fi
 kubectl apply -f https://projectcalico.docs.tigera.io/manifests/calico.yaml
 #kubectl apply -f https://raw.githubusercontent.com/cisco-open/kubei/main/deploy/kubei.yaml
