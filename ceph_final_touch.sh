0c1de5785901eecaee34f2a701443fe4e03fa550
gurubamal <99092495+gurubamal@users.noreply.github.com>
2022-06-05 23:23:37 +0530

    Update ceph_final_touch.sh

--- ceph_final_touch.sh
+++ ceph_final_touch.sh
@@ -1,1 +1,3 @@
-vagrant ssh node6  -c 'sudo bash  /vagrant/03ceph.sh'
+cat 03ceph.sh | vagrant ssh node6  -c 'sudo tee 03ceph.sh'
+cat 03ceph.sh | vagrant ssh node6  -c 'sudo chmod +x 03ceph.sh'
+vagrant ssh node6  -c 'sudo /vagrant/03ceph.sh'
