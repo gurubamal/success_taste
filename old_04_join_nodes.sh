d649fd8cdcd11b7131db40d206578d88b900b24a
gurubamal <99092495+gurubamal@users.noreply.github.com>
2022-06-06 13:59:21 +0530

    Delete old_04_join_nodes.sh

--- old_04_join_nodes.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/local/bin/bash
-
-echo $(cat join.info|grep kubeadm |cut -d'\' -f1) $(cat join.info |grep discovery-token-ca-cert-hash| cut -d"[" -f1) |tee compute_add.sh
-
-chmod +x compute_add.sh
