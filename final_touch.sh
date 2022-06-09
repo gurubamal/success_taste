9e336f1732dd59404f5cbbde9b50525815e44f4a
gurubamal <99092495+gurubamal@users.noreply.github.com>
2022-06-09 18:39:24 +0530

    Update final_touch.sh

--- final_touch.sh
+++ final_touch.sh
@@ -1,4 +1,4 @@
 cat 05_post_join_control.sh| vagrant ssh node6  -c 'sudo tee 05_post_join_control.sh'
 vagrant ssh node6  -c 'sudo chmod +x 05_post_join_control.sh'
 vagrant ssh node6  -c 'sudo ./05_post_join_control.sh'
-vagrant ssh node6  -c 'kubectl apply -f https://projectcalico.docs.tigera.io/manifests/calico.yaml'
+vagrant ssh node6  -c 'kubectl apply -f https://github.com/coreos/flannel/raw/master/Documentation/kube-flannel.yml'
