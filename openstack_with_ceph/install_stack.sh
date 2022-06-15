2f4abe8c35870e08df8f20edec0c4d2cdc6a6b6d
gurubamal <99092495+gurubamal@users.noreply.github.com>
2022-06-15 19:40:56 +0530

    Update install_stack.sh

--- openstack_with_ceph/install_stack.sh
+++ openstack_with_ceph/install_stack.sh
@@ -10,7 +10,7 @@ export LC_ALL=C
 #cp -ar openstack_with_ceph/files /root/
 cp -ar /vagrant/openstack_with_ceph/files /root/files
 cd  /root/files
-apt-get install -y net-tools software-properties-common
+#apt-get install -y net-tools software-properties-common
 interface=enp0s3
 ifconfig  $interface >| /root/$interface
 ip=`cat /root/$interface | grep inet | grep -v inet6 | awk '{print $2}'`
@@ -31,23 +31,23 @@ export DEBIAN_FRONTEND=noninteractive
 set +e
 for I in 1 2 3
 do
-    add-apt-repository -y cloud-archive:wallaby
+    add-#apt-repository -y cloud-archive:wallaby
     if [ $? == 0 ]
     then
         break
     fi
 done
 set -e
-#apt-mark hold ceph ceph-mgr-dashboard
-apt-get -y update && apt-get -y upgrade && apt-get -y dist-upgrade
-apt-get install -y python3-openstackclient python3-pip git
-apt-get install -y ssh-client
+##apt-mark hold ceph ceph-mgr-dashboard
+#apt-get -y update && #apt-get -y upgrade && #apt-get -y dist-upgrade
+#apt-get install -y python3-openstackclient python3-pip git
+#apt-get install -y ssh-client
 
 # install mysql
-#apt-key adv --fetch-keys 'https://mariadb.org/mariadb_release_signing_key.asc'
-#add-apt-repository 'deb [arch=amd64,arm64,ppc64el] https://mirror.vpsfree.cz/mariadb/repo/10.4/ubuntu bionic main'
-apt-get update && apt-get -y install mariadb-server
-apt-get -y install python3-pymysql && service mysql restart
+##apt-key adv --fetch-keys 'https://mariadb.org/mariadb_release_signing_key.asc'
+#add-#apt-repository 'deb [arch=amd64,arm64,ppc64el] https://mirror.vpsfree.cz/mariadb/repo/10.4/ubuntu bionic main'
+#apt-get update && #apt-get -y install mariadb-server
+#apt-get -y install python3-pymysql && service mysql restart
 echo "UPDATE mysql.user SET Password=PASSWORD('root') WHERE User='root';
 DELETE FROM mysql.user WHERE User='';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
@@ -60,7 +60,7 @@ cp /root/files/mysqld_openstack.cnf /etc/mysql/mariadb.conf.d/99-openstack.cnf
 service mysql restart
 
 # install rabbitmq
-apt-get -y install rabbitmq-server memcached python3-pymysql
+#apt-get -y install rabbitmq-server memcached python3-pymysql
 service rabbitmq-server start
 rabbitmqctl add_user openstack guru123
 rabbitmqctl set_permissions openstack ".*" ".*" ".*"
@@ -73,12 +73,12 @@ my_ip=`ifconfig $interface | grep "inet" | grep -v "inet6" | awk '{print $2}'`
 mysql -u root --password="guru123" -e "CREATE DATABASE keystone;"
 mysql -u root --password="guru123" -e "GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'localhost' IDENTIFIED BY 'guru123';"
 mysql -u root --password="guru123" -e "GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'%' IDENTIFIED BY 'guru123';"
-apt-get -y install apache2 
-apt-get -y install libapache2-mod-wsgi
-apt-get -y install memcached
-apt-get -y install python3-memcache
-apt-get -y install keystone
-apt-get -y install python3-openstackclient  libapache2-mod-wsgi-py3 python3-oauth2client
+#apt-get -y install apache2 
+#apt-get -y install libapache2-mod-wsgi
+#apt-get -y install memcached
+#apt-get -y install python3-memcache
+#apt-get -y install keystone
+#apt-get -y install python3-openstackclient  libapache2-mod-wsgi-py3 python3-oauth2client
 
 cp /root/files/keystone.conf /etc/keystone/
 cp /root/files/keystone.policy.yaml /etc/keystone/
@@ -122,7 +122,7 @@ openstack endpoint create --region RegionOne image public http://$my_ip:9292
 openstack endpoint create --region RegionOne image internal http://$my_ip:9292
 openstack endpoint create --region RegionOne image admin http://$my_ip:9292
 
-apt-get -y install glance
+#apt-get -y install glance
 cp /root/files/glance-api.conf /etc/glance/
 #cp /root/files/glance-registry.conf /etc/glance/
 su -s /bin/sh -c "glance-manage db_sync" glance
@@ -140,7 +140,7 @@ openstack endpoint create --region RegionOne placement public http://$my_ip:8778
 openstack endpoint create --region RegionOne placement internal http://$my_ip:8778
 openstack endpoint create --region RegionOne placement admin http://$my_ip:8778
 
-apt-get -y install placement-api
+#apt-get -y install placement-api
 cp /root/files/placement.conf /etc/placement/
 su -s /bin/sh -c "placement-manage db sync" placement
 service apache2 restart
@@ -158,7 +158,7 @@ mysql -u root --password="guru123" -e "CREATE DATABASE cinder;"
 mysql -u root --password="guru123" -e "GRANT ALL PRIVILEGES ON cinder.* TO 'cinder'@'localhost' IDENTIFIED BY 'guru123';"
 mysql -u root --password="guru123" -e "GRANT ALL PRIVILEGES ON cinder.* TO 'cinder'@'%' IDENTIFIED BY 'guru123';
 
-apt -y install cinder-api cinder-scheduler python3-cinderclient
+#apt -y install cinder-api cinder-scheduler python3-cinderclient
 mv /etc/cinder/cinder.conf /etc/cinder/cinder.conf.org
 cp /root/files/cinder.conf  /etc/cinder/
 chgrp cinder /etc/cinder/cinder.conf
@@ -191,9 +191,9 @@ openstack endpoint create --region RegionOne compute public http://$my_ip:8774/v
 openstack endpoint create --region RegionOne compute internal http://$my_ip:8774/v2.1
 openstack endpoint create --region RegionOne compute admin http://$my_ip:8774/v2.1
 
-apt -y install nova-api nova-conductor nova-scheduler nova-novncproxy placement-api python3-novaclient
-apt -y install nova-compute nova-compute-kvm qemu-system-data qemu-kvm libvirt-daemon-system libvirt-daemon virtinst bridge-utils libosinfo-bin libguestfs-tools virt-top
-#apt-get -y install nova-api nova-conductor nova-novncproxy nova-scheduler
+#apt -y install nova-api nova-conductor nova-scheduler nova-novncproxy placement-api python3-novaclient
+#apt -y install nova-compute nova-compute-kvm qemu-system-data qemu-kvm libvirt-daemon-system libvirt-daemon virtinst bridge-utils libosinfo-bin libguestfs-tools virt-top
+##apt-get -y install nova-api nova-conductor nova-novncproxy nova-scheduler
 cp /root/files/nova.conf /etc/nova/
 cp /root/files/nova.policy.yaml /etc/nova/
 sed -i s/MY_IP/$my_ip/g /etc/nova/nova.conf
@@ -208,7 +208,7 @@ service nova-scheduler restart
 service nova-conductor restart
 service nova-novncproxy restart
 #nova compute
-apt-get install -y nova-compute
+#apt-get install -y nova-compute
 service nova-compute restart
 openstack hypervisor list
 su -s /bin/sh -c "nova-manage cell_v2 discover_hosts --verbose" nova
@@ -225,8 +225,8 @@ openstack service create --name neutron --description "OpenStack Networking" net
 openstack endpoint create --region RegionOne network public http://$my_ip:9696
 openstack endpoint create --region RegionOne network internal http://$my_ip:9696
 openstack endpoint create --region RegionOne network admin http://$my_ip:9696
-#apt-get -y install neutron-server neutron-plugin-ml2 neutron-linuxbridge-agent neutron-dhcp-agent neutron-metadata-agent neutron-l3-agent
-apt-get -y install install neutron-server neutron-plugin-ml2 neutron-linuxbridge-agent neutron-l3-agent neutron-dhcp-agent neutron-metadata-agent python3-neutronclient 
+##apt-get -y install neutron-server neutron-plugin-ml2 neutron-linuxbridge-agent neutron-dhcp-agent neutron-metadata-agent neutron-l3-agent
+#apt-get -y install install neutron-server neutron-plugin-ml2 neutron-linuxbridge-agent neutron-l3-agent neutron-dhcp-agent neutron-metadata-agent python3-neutronclient 
 cp /root/files/radvd.conf /etc/
 cp /root/files/neutron.conf /etc/neutron/
 cp /root/files/neutron.policy.json /etc/neutron/
@@ -250,7 +250,7 @@ service nova-compute restart
 
 # dashboard
 
-apt-get install -y openstack-dashboard
+#apt-get install -y openstack-dashboard
 cp /root/files/local_settings.py /etc/openstack-dashboard/local_settings.py
 chown www-data /var/lib/openstack-dashboard/secret_key
 service apache2 reload
@@ -276,13 +276,18 @@ openstack role add --domain heat --user-domain heat --user heat_domain_admin adm
 openstack role create heat_stack_owner
 openstack role add --project demo --user demo heat_stack_owner
 openstack role create heat_stack_user
-apt-get install heat-api heat-api-cfn heat-engine python3-zunclient python3-vitrageclient -y
+#apt-get install heat-api heat-api-cfn heat-engine python3-zunclient python3-vitrageclient -y
 cp /root/files/heat.conf /etc/heat/
 #su -s /bin/sh -c "heat-manage db_sync" heat
-su -s /bin/bash heat -c "heat-manage db_sync"
+#su -s /bin/bash heat -c "heat-manage db_sync"
 
 service heat-api restart
 service heat-api-cfn restart
 service heat-engine restart
-
 chown horizon /var/lib/openstack-dashboard/secret_key
+echo "{
+  "os_compute_api:os-extended-server-attributes": "rule:admin_or_owner",
+}" | tee /etc/nova/policy.json
+chgrp nova /etc/nova/policy.json
+chmod 640 /etc/nova/policy.json
+systemctl restart nova-api
