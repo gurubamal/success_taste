81d76811bbccdcbbbad29f82e82a4f54dee44894
gurubamal <99092495+gurubamal@users.noreply.github.com>
2022-06-15 17:41:57 +0530

    Delete openstack_with_ceph/files directory

--- openstack_with_ceph/files/lab_openrc.sh
+++
@@ -1,8 +0,0 @@
-export OS_USERNAME=admin
-export OS_AUTH_URL=http://openstack-controller.avi.local:5000/v3
-export OS_PASSWORD=avi123
-export OS_PROJECT_DOMAIN_NAME=default
-export OS_USER_DOMAIN_NAME=default
-export OS_PROJECT_NAME=admin
-export OS_IDENTITY_API_VERSION=3
-export OS_IMAGE_API_VERSION=2
