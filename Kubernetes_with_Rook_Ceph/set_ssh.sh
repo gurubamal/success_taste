5c2fa2e30eca845dbfa881787323388a3f9955c3
gurubamal <99092495+gurubamal@users.noreply.github.com>
2022-06-20 17:27:47 +0530

    Update set_ssh.sh

--- Kubernetes_with_Rook_Ceph/set_ssh.sh
+++ Kubernetes_with_Rook_Ceph/set_ssh.sh
@@ -5,7 +5,7 @@ if ! grep 192.168.58 /etc/hosts
         echo 192.168.58.7        node7   node02| sudo tee -a /etc/hosts
         echo 192.168.58.8        node8   node03| sudo tee -a /etc/hosts
         echo 192.168.58.6        node6   node01| sudo tee -a /etc/hosts
-        echo 192.168.58.5        node5  controller| sudo tee -a /etc/hosts
+        echo 192.168.58.9        node9  controller| sudo tee -a /etc/hosts
 fi
 
 if ! grep "PasswordAuthentication yes" /etc/ssh/sshd_config
