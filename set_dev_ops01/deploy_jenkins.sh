1c1d5d0c15e85021ed2bbca4e24cc7fb65415266
gurubamal <99092495+gurubamal@users.noreply.github.com>
2022-06-21 14:31:25 +0530

    Update deploy_jenkins.sh

--- set_dev_ops01/deploy_jenkins.sh
+++ set_dev_ops01/deploy_jenkins.sh
@@ -1,3 +1,3 @@
-cat jenkins_install.sh| vagrant ssh node6  -c 'tee jenkins_install.sh'
+cat jenkins_install.sh| vagrant ssh node6  -c 'sudo tee jenkins_install.sh'
 vagrant ssh node6  -c 'sudo chmod +x jenkins_install.sh'
 vagrant ssh node6  -c './jenkins_install.sh'
