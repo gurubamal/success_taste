32faed6904fb999546c183ca7a0902c0fa7ea5b3
gurubamal <99092495+gurubamal@users.noreply.github.com>
2022-08-04 22:22:47 +0530

    Add files via upload

--- kind_deploy/kind.sh
+++ kind_deploy/kind.sh
@@ -1,11 +1,18 @@
 sudo apt install docker.io
 sudo apt install golang-go
-curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.14.0/kind-linux-amd64
-sudo chmod +x kind
-sudo mv kind /usr/bin/.
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-sudo chmod +x kubectl
-sudo mv kubectl /usr/bin/
+if ! ls /usr/bin/kind
+	then 
+	curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.14.0/kind-linux-amd64
+	sudo chmod +x kind
+	sudo mv kind /usr/bin/.
+fi
+
+if ! ls /usr/bin/kubectl
+	then
+	curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+	sudo chmod +x kubectl
+	sudo mv kubectl /usr/bin/
+fi
 if ! ls kind-demo/
 	then
 	git clone https://github.com/vfarcic/kind-demo.git
