c5631deaa59b46881cd9506311d345723c11123a
gurubamal <99092495+gurubamal@users.noreply.github.com>
2022-08-22 20:44:22 +0530

    Add files via upload

--- CKS-Practice-Environment/finish_it.sh
+++ CKS-Practice-Environment/finish_it.sh
@@ -8,7 +8,10 @@ if [ "$HOSTNAME" = node6 ]; then
         sudo cp -i /etc/kubernetes/admin.conf /root/.kube/config
         sudo chown vagrant:vagrant  /home/vagrant/.kube/config
 	sleep 120
-        kubectl create -f https://projectcalico.docs.tigera.io/manifests/tigera-operator.yaml
-        kubectl create -f https://projectcalico.docs.tigera.io/manifests/custom-resources.yaml
+	kubectl apply -f /vagrant/calico.yaml
+	#sudo kubectl apply -f /vagrant/tigera-operator.yaml
+	#sudo kubectl apply -f /vagrant/custom-resources.yaml
+        #kubectl create -f https://projectcalico.docs.tigera.io/manifests/tigera-operator.yaml
+        #kubectl create -f https://projectcalico.docs.tigera.io/manifests/custom-resources.yaml
         fi
 fi
