a24ec1bb0589a464913a9e370584c1005fa64565
gurubamal <99092495+gurubamal@users.noreply.github.com>
2023-06-03 22:22:06 +0530

    Delete Setup-node-5-6 directory

--- Setup-node-5-6/compute_add.sh
+++
@@ -1,9 +0,0 @@
-if ! [ $HOSTNAME = node5 ]; then
-	if ! [ -e /etc/kubernetes/kubelet.conf ] ; then
-	sudo rm /etc/containerd/config.toml
-	sudo systemctl restart containerd
-	sudo systemctl enable containerd
-	sudo kubeadm join 192.168.58.5:6443 --token qesudx.qyg2owtx0t4dmio3 --discovery-token-ca-cert-hash sha256:d2f7d34bf6b589ddaefc7b91ea9f59547b35f456df92cf02575f2b53c8735352
-	fi
-	else exit 0
-	fi
--- Setup-node-5-6/init_control.sh
+++
@@ -1,1 +0,0 @@
-ansible -i hosts control  -u vagrant  -a "sudo kubeadm init --apiserver-advertise-address=10.0.3.6 --pod-network-cidr=10.244.0.0/16" |tee init.txt ; echo sudo $(tail -2 init.txt|head -1| cut -d'\' -f1)  $(tail -1 init.txt| cut -d'[' -f1) |tee compute_add.sh
