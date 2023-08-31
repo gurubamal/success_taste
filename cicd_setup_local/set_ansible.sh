b57d430eed6d3e96674027c383425fff7a29f9eb
gurubamal <99092495+gurubamal@users.noreply.github.com>
2023-08-31 14:06:34 +0530

    Update set_ansible.sh

--- cicd_setup_local/set_ansible.sh
+++ cicd_setup_local/set_ansible.sh
@@ -15,5 +15,22 @@ ansible_python_interpreter=/usr/bin/python3" | sudo tee /etc/ansible/hosts
 #ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa <<<y >/dev/null 2>&1
 
 #for i in 6 7 8
-#	do	/vagrant/login_expect  node$i
+#       do      /vagrant/login_expect  node$i
 #done
+sudo apt update
+sudo apt -y remove containerd.io
+sudo apt -y install docker.io
+sudo apt -y autoremove
+Sudo mkdir /etc/docker/registry
+echo "version: 0.1
+storage:
+  filesystem:
+    rootdirectory: /var/lib/registry
+"   |sudo tee  /etc/docker/registry/config.yml
+sudo usermod -aG docker vagrant
+sudo systemctl unmask docker.service
+sudo systemctl enable docker.service
+sudo systemctl start docker.service
+
+docker run -d -p 5000:5000 --restart=always --name registry registry:2
+docker ps
