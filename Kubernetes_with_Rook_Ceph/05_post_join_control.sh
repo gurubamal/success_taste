e0beaeb302139407fdff6d382c4bdb67b6c2a0e4
gurubamal <99092495+gurubamal@users.noreply.github.com>
2024-06-05 13:30:51 +0530

    Update 05_post_join_control.sh

--- Kubernetes_with_Rook_Ceph/05_post_join_control.sh
+++ Kubernetes_with_Rook_Ceph/05_post_join_control.sh
@@ -1,19 +1,39 @@
-#kubectl apply -f https://github.com/coreos/flannel/raw/master/Documentation/kube-flannel.yml
-#kubectl apply -f kubei.yaml
-#if [ "$HOSTNAME" = node6 ]; then
-#        if ! [ -e /home/vagrant/.kube/config ]
-#        then
-        sudo mkdir -p /home/vagrant/.kube
-	sudo chown vagrant:vagrant /home/vagrant/.kube
-        sudo cp -i /etc/kubernetes/admin.conf /home/vagrant/.kube/config
-        sudo chown vagrant:vagrant  /home/vagrant/.kube/config
-#echo "sleeping for 2 minutes"
-#sleep 120
-	sudo snap install helm --classic
-#        kubectl apply -f https://projectcalico.docs.tigera.io/manifests/calico.yaml
-#sudo snap install helm --classic
-#       else echo "Now Installing calico Netwrok Plugin......."
-#        fi
-#fi
-#kubectl apply -f https://projectcalico.docs.tigera.io/manifests/calico.yaml
-#kubectl apply -f https://raw.githubusercontent.com/cisco-open/kubei/main/deploy/kubei.yaml
+#!/bin/bash
+
+# Authored by: Ram Nath Bamal (Guru)
+
+# Create .kube directory for vagrant user if not exists and copy the Kubernetes admin config
+sudo mkdir -p /home/vagrant/.kube
+if [ $? -ne 0 ]; then
+  echo "Error: Failed to create /home/vagrant/.kube directory."
+fi
+
+sudo chown vagrant:vagrant /home/vagrant/.kube
+if [ $? -ne 0 ]; then
+  echo "Error: Failed to change ownership of /home/vagrant/.kube directory."
+fi
+
+sudo cp -i /etc/kubernetes/admin.conf /home/vagrant/.kube/config
+if [ $? -ne 0 ]; then
+  echo "Error: Failed to copy admin.conf to /home/vagrant/.kube/config."
+fi
+
+sudo chown vagrant:vagrant /home/vagrant/.kube/config
+if [ $? -ne 0 ]; then
+  echo "Error: Failed to change ownership of /home/vagrant/.kube/config."
+fi
+
+# Install Helm
+sudo snap install helm --classic
+if [ $? -ne 0 ]; then
+  echo "Error: Failed to install Helm."
+fi
+
+# Uncomment the following lines to apply network plugin and other configurations
+# kubectl apply -f https://projectcalico.docs.tigera.io/manifests/calico.yaml
+# sudo snap install helm --classic
+# else echo "Now Installing calico Network Plugin......."
+# fi
+# fi
+# kubectl apply -f https://projectcalico.docs.tigera.io/manifests/calico.yaml
+# kubectl apply -f https://raw.githubusercontent.com/cisco-open/kubei/main/deploy/kubei.yaml
