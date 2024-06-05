c0ba48b3ad6aa75e01d8934df5373fb05bf16e34
gurubamal <99092495+gurubamal@users.noreply.github.com>
2024-06-05 13:11:43 +0530

    Update compute_add.sh

--- CKS-Practice-Environment/compute_add.sh
+++ CKS-Practice-Environment/compute_add.sh
@@ -1,9 +1,30 @@
-if ! [ $HOSTNAME = node6 ]; then
-	if ! [ -e /etc/kubernetes/kubelet.conf ] ; then
-	sudo rm /etc/containerd/config.toml
-	sudo systemctl restart containerd
-	sudo systemctl enable containerd
-	sudo kubeadm join 192.168.58.6:6443 --token a874ub.qdhegpl2x4m369sc --discovery-token-ca-cert-hash sha256:97362483b6742c804b092fb68d968eb526d29ddfaecc42339255cf53af79a11c
-	fi
-	else exit 0
-	fi
+#!/bin/bash
+
+# Authored by: Ram Nath Bamal (Guru)
+
+# Ensure the script runs only on nodes other than node6
+if ! [ "$HOSTNAME" = "node6" ]; then
+    if ! [ -e /etc/kubernetes/kubelet.conf ]; then
+        sudo rm /etc/containerd/config.toml
+        if [ $? -ne 0 ]; then
+            echo "Error: Failed to remove /etc/containerd/config.toml."
+        fi
+
+        sudo systemctl restart containerd
+        if [ $? -ne 0 ]; then
+            echo "Error: Failed to restart containerd."
+        fi
+
+        sudo systemctl enable containerd
+        if [ $? -ne 0 ]; then
+            echo "Error: Failed to enable containerd."
+        fi
+
+        sudo kubeadm join 192.168.58.6:6443 --token a874ub.qdhegpl2x4m369sc --discovery-token-ca-cert-hash sha256:97362483b6742c804b092fb68d968eb526d29ddfaecc42339255cf53af79a11c
+        if [ $? -ne 0 ]; then
+            echo "Error: Failed to join Kubernetes cluster."
+        fi
+    fi
+else
+    exit 0
+fi
