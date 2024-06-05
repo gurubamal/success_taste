dad9826e989572ae84bea1b84de5e5c1194fdbf5
gurubamal <99092495+gurubamal@users.noreply.github.com>
2024-06-05 13:36:32 +0530

    Update execute.sh

--- Kubernetes_with_Rook_Ceph/execute.sh
+++ Kubernetes_with_Rook_Ceph/execute.sh
@@ -1,21 +1,83 @@
-#RUN on controlnode:
-#01#>~/.ssh/known_hosts
-#02#ssh-copy-id vagrant@10.0.3.6
-#03#ssh-copy-id vagrant@10.0.3.7
-#04#ssh-copy-id vagrant@10.0.3.8
+#!/bin/bash
 
+# Authored by: Ram Nath Bamal (Guru)
+
+# Clear known hosts file on control node
+> ~/.ssh/known_hosts
+if [ $? -ne 0 ]; then
+  echo "Error: Failed to clear ~/.ssh/known_hosts."
+fi
+
+# Copy SSH keys to compute nodes
+ssh-copy-id vagrant@10.0.3.6
+if [ $? -ne 0 ]; then
+  echo "Error: Failed to copy SSH key to 10.0.3.6."
+fi
+
+ssh-copy-id vagrant@10.0.3.7
+if [ $? -ne 0 ]; then
+  echo "Error: Failed to copy SSH key to 10.0.3.7."
+fi
+
+ssh-copy-id vagrant@10.0.3.8
+if [ $? -ne 0 ]; then
+  echo "Error: Failed to copy SSH key to 10.0.3.8."
+fi
+
+# Run prepare_local.sh script
 ./prepare_local.sh
+if [ $? -ne 0 ]; then
+  echo "Error: Failed to run prepare_local.sh."
+fi
 
-#05#ansible-playbook -i hosts -u vagrant reboot.yml
+# Run ansible playbook to reboot nodes
+ansible-playbook -i hosts -u vagrant reboot.yml
+if [ $? -ne 0 ]; then
+  echo "Error: Failed to run ansible playbook for rebooting nodes."
+fi
 
-#06 #ansible -i hosts control  -u vagrant  -a "sudo kubeadm init --apiserver-advertise-address=10.0.3.6 --pod-network-cidr=10.244.0.0/16" |tee init.txt ; echo sudo $(tail -2 init.txt|head -1| cut -d'\' -f1)  $(tail -1 init.txt| cut -d'[' -f1) |tee compute_add.sh
+# Initialize Kubernetes on control node and generate join command
+ansible -i hosts control -u vagrant -a "sudo kubeadm init --apiserver-advertise-address=10.0.3.6 --pod-network-cidr=10.244.0.0/16" | tee init.txt
+if [ $? -ne 0 ]; then
+  echo "Error: Failed to initialize Kubernetes on control node."
+fi
 
+JOIN_CMD=$(tail -2 init.txt | head -1 | cut -d'\' -f1) $(tail -1 init.txt | cut -d'[' -f1)
+echo "sudo $JOIN_CMD" | tee compute_add.sh
+if [ $? -ne 0 ]; then
+  echo "Error: Failed to generate join command."
+fi
 
+# Run init_control.sh script
 ./init_control.sh
+if [ $? -ne 0 ]; then
+  echo "Error: Failed to run init_control.sh."
+fi
 
+# Run ansible playbooks to configure Kubernetes cluster
 ansible-playbook -i hosts -u vagrant kubernetes.yaml
+if [ $? -ne 0 ]; then
+  echo "Error: Failed to run ansible playbook for kubernetes.yaml."
+fi
+
 ansible-playbook -i hosts -u vagrant join-compute.yaml
+if [ $? -ne 0 ]; then
+  echo "Error: Failed to run ansible playbook for join-compute.yaml."
+fi
+
 ansible-playbook -i hosts -u vagrant helm.yml
-#09 Run on control node  #sudo  kubectl apply -f https://github.com/coreos/flannel/raw/master/Documentation/kube-flannel.yml 
-#https://docs.projectcalico.org/v3.14/manifests/calico.yaml
-#kubectl apply -f https://docs.projectcalico.org/v3.14/manifests/calico.yaml
+if [ $? -ne 0 ]; then
+  echo "Error: Failed to run ansible playbook for helm.yml."
+fi
+
+# Apply network plugin on control node
+kubectl apply -f https://github.com/coreos/flannel/raw/master/Documentation/kube-flannel.yml
+if [ $? -ne 0 ]; then
+  echo "Error: Failed to apply Flannel network plugin."
+fi
+
+# Optionally, apply Calico network plugin
+# kubectl apply -f https://docs.projectcalico.org/v3.14/manifests/calico.yaml
+# if [ $? -ne 0 ]; then
+#   echo "Error: Failed to apply Calico network plugin."
+# fi
