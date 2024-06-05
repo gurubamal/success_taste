06654339b8ffb1aab6d976430ae2973588d70d8d
gurubamal <99092495+gurubamal@users.noreply.github.com>
2024-06-05 18:02:47 +0530

    Update 02_setup.sh

--- cicd_setup_local/02_setup.sh
+++ cicd_setup_local/02_setup.sh
@@ -31,7 +31,7 @@ sudo apt-get install -y apt-transport-https ca-certificates curl software-proper
 
 # Add Docker repository and key
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
 # Install Docker
 sudo apt-get update
