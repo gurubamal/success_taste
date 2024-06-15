27f8560561ae809b81608617fb93c5c9ac25258a
gurubamal <99092495+gurubamal@users.noreply.github.com>
2024-06-15 19:52:55 +0530

    Update docker_login.sh

--- cicd_ready_envt/scripts/docker_login.sh
+++ cicd_ready_envt/scripts/docker_login.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-# Step 1: Install Docker using Snap if not already installed
-if ! snap list | grep -q docker; then
-  sudo snap install docker
+# Step 1: Install Docker using apt if not already installed
+if ! command -v docker &> /dev/null; then
+  echo "Docker is not installed. Installing Docker..."
+  sudo apt-get update
+  sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+  sudo apt-get update
+  sudo apt-get install -y docker-ce
 else
   echo "Docker is already installed."
 fi
